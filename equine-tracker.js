a67b8bb636e0b2bf43382f908742b1351b5caf36
Steve Loar <sjl7909@gmail.com>
2015-01-28 11:45:36 -0500

    Playing with routes and controllers

--- equine-tracker.js
+++ equine-tracker.js
@@ -1,23 +1,59 @@
-if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault("counter", 0);
+Iron.utils.debug = true;
 
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get("counter");
+Horses = new Mongo.Collection("horses");
+
+Router.route('/', {
+  controller: 'HorseController',
+  action: 'index'
+  //name: 'home'
+});
+
+Router.route('/horses', {
+  controller: 'HorseController',
+  action: 'index'
+});
+
+Router.route('/horses/:_id', {
+  controller: 'HorseController',
+  action: 'show'
+});
+
+if (Meteor.isClient) {
+  Meteor.subscribe("horses");
+
+  ApplicationController = RouteController.extend({
+    layoutTemplate: 'AppLayout',
+
+/*    onBeforeAction: function () {
+      console.log('app before hook!');
+      this.next();
+    },
+*/
+    action: function () {
+      console.log('this should be overridden!');
     }
   });
 
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set("counter", Session.get("counter") + 1);
+  HorseController = ApplicationController.extend({
+
+    show: function () {
+      var id = this.params._id;
+      this.render('HorseShow', {
+        data: {
+          horse_id: this.params._id
+        }
+      });
+    },
+
+    index: function () {
+      this.render('HorseIndex');
     }
+
   });
 }
 
 if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
+  Meteor.publish("horses", function () {
+    return Horses.find( );
   });
 }
