dbe2c056ce21a3488cdb73b76b7497553af7e962
Steve Loar <sjl7909@gmail.com>
2015-02-02 14:44:35 -0500

    top level route in place

--- server/fixtures.js
+++ server/fixtures.js
@@ -2,42 +2,42 @@ if (Horses.find().count() === 0) {
   Horses.insert({
     name: 'Dunny',
     breed: 'Quarter Horse',
-    imageURL: 'horse1',
-    thumbURL: 'horsethumb1'
+    imageURL: 'horse1.jpg',
+    thumbURL: 'horsethumb1.jpg'
   });
 
   Horses.insert({
     name: 'Sam',
     breed: 'Quarter Horse',
-    imageURL: 'horse2',
-    thumbURL: 'horsethumb2'
+    imageURL: 'horse2.jpg',
+    thumbURL: 'horsethumb2.jpg'
   });
 
   Horses.insert({
     name: 'Gamble',
     breed: 'Arabian',
-    imageURL: 'horse3',
-    thumbURL: 'horsethumb3'
+    imageURL: 'horse3.jpg',
+    thumbURL: 'horsethumb3.jpg'
   });
 
   Horses.insert({
     name: 'Sultan',
     breed: 'Quarter Horse',
-    imageURL: 'horse4',
-    thumbURL: 'horsethumb4'
+    imageURL: 'horse4.jpg',
+    thumbURL: 'horsethumb4.jpg'
   });
 
   Horses.insert({
     name: 'Memphis',
     breed: 'Throughbred',
-    imageURL: 'horse5',
-    thumbURL: 'horsethumb5'
+    imageURL: 'horse5.jpg',
+    thumbURL: 'horsethumb5.jpg'
   });
 
   Horses.insert({
     name: 'Zanny',
     breed: 'Belgian',
-    imageURL: 'horse6',
-    thumbURL: 'horsethumb6'
+    imageURL: 'horse6.jpg',
+    thumbURL: 'horsethumb6.jpg'
   });
 }
